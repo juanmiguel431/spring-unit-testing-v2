b2bc0df91c3462324d3be5a45781804b169a2625
juanmiguel431 <juanmiguel431@gmail.com>
2024-11-26 20:50:03 -0400

    Initial commit

---
+++ src/main/java/jmpc/unittesting/springdemo/SpringUnitTestingV2Application.java
@@ -0,0 +1,13 @@
+package jmpc.unittesting.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUnitTestingV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringUnitTestingV2Application.class, args);
+  }
+
+}
---
+++ src/test/java/jmpc/unittesting/springdemo/SpringUnitTestingV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jmpc.unittesting.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnitTestingV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
