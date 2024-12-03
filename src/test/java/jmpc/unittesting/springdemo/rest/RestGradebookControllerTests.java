a0f0b6679bfa0b21b062518f0e466238f9eeee49
juanmiguel431 <juanmiguel431@gmail.com>
2024-12-02 22:59:19 -0400

    RestGradebookControllerTests - getStudentsHttpRequest test added.

--- src/test/java/jmpc/unittesting/springdemo/rest/RestGradebookControllerTests.java
+++ src/test/java/jmpc/unittesting/springdemo/rest/RestGradebookControllerTests.java
@@ -1,23 +1,32 @@
 package jmpc.unittesting.springdemo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import jmpc.unittesting.springdemo.models.entities.CollegeStudent;
 import jmpc.unittesting.springdemo.repositories.HistoryGradeRepository;
 import jmpc.unittesting.springdemo.repositories.MathGradeRepository;
 import jmpc.unittesting.springdemo.repositories.ScienceGradeRepository;
 import jmpc.unittesting.springdemo.repositories.StudentRepository;
 import jmpc.unittesting.springdemo.services.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @TestPropertySource("/application-test.properties")
 @AutoConfigureMockMvc
@@ -51,6 +60,22 @@ public class RestGradebookControllerTests {
   @Mock
   private StudentAndGradeService studentAndGradeServiceMock;
 
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private static final MediaType APPLICATION_JSON = MediaType.APPLICATION_JSON;
+
+  @BeforeAll
+  public static void beforeAll() {
+    request = new MockHttpServletRequest();
+    request.setParameter("firstname", "Firstname");
+    request.setParameter("lastname", "Lastname");
+    request.setParameter("email", "user@test.com");
+  }
+
   @BeforeEach
   public void beforeEach() {
     jdbcTemplate.execute("ALTER TABLE students ALTER COLUMN ID RESTART WITH 1;");
@@ -72,8 +97,18 @@ public class RestGradebookControllerTests {
     jdbcTemplate.execute("delete from science_grades");
   }
 
+  /// https://github.com/json-path/JsonPath
   @Test
-  public void placeholder() {
+  public void getStudentsHttpRequest() throws Exception {
 
+    var student = new CollegeStudent("User1", "", "user1@test.com");
+    entityManager.persist(student);
+    entityManager.flush();
+
+    var result = mockMvc.perform(MockMvcRequestBuilders.get("/api/gradebook/"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(APPLICATION_JSON))
+        .andExpect(jsonPath("$", hasSize(2)))
+        .andReturn();
   }
 }
